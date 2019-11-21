91bb146953324c7650a3707c3e66ba95f9ffdca8
Georgi Manov <georgimanov@gmail.com>
2019-11-21 20:33:50 +0200

    Initial commit

---
+++ MyWebApp/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace MyWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string myString, DateTime myDate)
+        {
+            return this.Json(new { date = myDate, value = $"from controller: {myString} - {myDate}" });
+        }
+    }
+}
